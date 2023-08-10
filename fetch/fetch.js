716fcba7d5c674369dd4391504a62c1de28976a1
strawhatrag <sreeragbharathan791@gmail.com>
2023-08-10 16:22:56 +0530

    fetch end

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,9 +1,16 @@
-const url = "http://worldtimeapi.org/api/timezone/America/New_York";
+const url =
+  "https://api.spotify.com/v1/artists/2CIMQHirSU0MQqyYHq0eOx,57dN52uHvrHOxijzpIgu3E,1vCWHaC5f2uS3yhpwWbIA6";
 
 async function getData() {
-  const response = await fetch(url);
-  const data = await response.json(); // cus .json is a promise
-  console.log(data);
+  const request = new Request(url, {
+    headers: {
+      Authorization: "Bearer 12",
+    },
+  });
+  const response = fetch(request);
+
+  //const data = await response.json(); // cus .json is a promise
+  console.log(response);
 }
 
 getData();
